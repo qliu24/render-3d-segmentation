d4977bbc30469604653e55ac392726ecae6906c7
qliu24 <qingliu@jhu.edu>
2019-12-03 15:47:22 -0500

    fix file names

--- render_manual_anno_parts.py
+++ render_manual_anno_parts.py
@@ -7,7 +7,7 @@ sys.path.append(BASE_DIR)
 
 blender_executable_path = '/mnt/1TB_SSD/qing/blender-2.79b-linux-glibc219-x86_64/blender'
 blank_file = osp.join(BASE_DIR, 'blank.blend')
-render_code = 'blender_hand_anno_parts.py'
+render_code = 'blender_manual_anno_parts.py'
     
 
 def render(img_savedir, anno_savedir, model_id, model_dir, anno_dir, par_file):
